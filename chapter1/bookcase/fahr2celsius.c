589a52819ad55fc5b58b48d5ef5e2d4fdbd86df3
samuel-tuo <samuel.tuo@yahoo.com>
2021-05-27 12:50:28 +0800

    fahr to celsius for integer

---
+++ chapter1/bookcase/fahr2celsius.c
@@ -0,0 +1,22 @@
+//"华氏度和摄氏度温度对照表"
+#include <stdio.h>
+
+int main()
+{
+	int fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = 5 * (fahr - 32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr += step;
+	}
+	return 0;
+}
+
+
