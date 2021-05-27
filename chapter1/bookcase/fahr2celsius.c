b92bbf3478fab6f1e4cabe0c3e43b70afe7b9d59
samuel-tuo <samuel.tuo@yahoo.com>
2021-05-27 12:56:26 +0800

    打印区域右对齐

--- chapter1/bookcase/fahr2celsius.c
+++ chapter1/bookcase/fahr2celsius.c
@@ -13,7 +13,7 @@ int main()
 	fahr = lower;
 	while (fahr <= upper) {
 		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		printf("%3d\t%3d\n", fahr, celsius);
 		fahr += step;
 	}
 	return 0;
